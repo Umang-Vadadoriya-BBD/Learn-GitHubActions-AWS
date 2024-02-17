6ae441d66e09af2d1677681ac6cc659f6ad43007
Umang Vadadoriya <umang.vadadoriya@bbd.co.za>
2024-02-17 23:23:46 +0530

    Adding New Column

--- changelog.sql
+++ changelog.sql
@@ -2,3 +2,6 @@
 
 -- changeset liquibase:1
 CREATE TABLE liquibaseTB (test_id INT, test_column VARCHAR(255), PRIMARY KEY (test_id))
+
+-- changeset liquibase:2
+alter table liquibaseTB add newColumn int;
